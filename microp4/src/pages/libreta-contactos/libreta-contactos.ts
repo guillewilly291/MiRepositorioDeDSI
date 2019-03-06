ac4e75fcc12be173cbd2873097e3640522432be8
Guillermo <guillermodelecea@outlook.es>
2019-03-06 12:30:05 +0100

    microp5

---
+++ microp4/src/pages/libreta-contactos/libreta-contactos.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NuevoContactoPage } from '../nuevo-contacto/nuevo-contacto';
+import { Contact } from '../../models/contact.model';
+import { ContactService } from '../../services/contact.service';
+@IonicPage()
+@Component({
+selector: 'page-libreta-contactos',
+templateUrl: 'libreta-contactos.html',
+})
+export class LibretaContactosPage {
+contacts: Contact[] =[];
+constructor(public navCtrl: NavController, public navParams: NavParams, private ContactService:ContactService) {
+ }
+ionViewWillEnter(){
+ this.contacts = this.ContactService.getContacts();
+ }
+onLoadContactosPage(){
+ this.navCtrl.push(NuevoContactoPage);
+ }
+}
